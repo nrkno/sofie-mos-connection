8ea405cebeb876f31c7c1d61290c67a20da7621b
Jesper Stærkær <jesper@starkar.no>
2018-02-23 21:22:01 +0100

    fix(mosString128): Changed "set" to "string" member. Refactored validation

--- src/dataTypes/mosString128.ts
+++ src/dataTypes/mosString128.ts
@@ -2,22 +2,24 @@ export default class MosString128 {
 
   private _str: string
 
-    /** */
+  /** */
   constructor (str: string) {
-
-    this._str = str
-
-    this.validate()
+    this.string = str
   }
 
+  /** */
   toString (): string {
     return this._str
   }
-  set set (str: string) {
+
+  /** */
+  set string (str: string) {
     this._str = str
-    this.validate()
+    this._validate()
   }
-  private validate () {
+
+  /** */
+  private _validate () {
     if ((this._str || '').length > 128) throw new Error('String length is too long!')
   }
 }
