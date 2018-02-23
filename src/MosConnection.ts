ddd621da11c0221683d829fa20eb7f5af7e6a3ce
Jesper Stærkær <jesper@starkar.no>
2018-02-23 16:46:16 +0100

    chore: linting

--- src/MosConnection.ts
+++ src/MosConnection.ts
@@ -1,14 +1,18 @@
-import ConnectionConfig from './config/ConnectionConfig';
+import ConnectionConfig from './config/ConnectionConfig'
+import MosSocketClient from './socket/mosSocketClient'
+import MosSocketServer from './socket/mosSocketServer'
 
 export default class MosConnection {
-    
-    private _conf: ConnectionConfig
 
-    /** */
-    constructor (conf: ConnectionConfig) {
-        this._conf = conf
-    }
+  private _conf: ConnectionConfig
 
-    /** */
-    
- }
+  private _lowerSocket: MosSocketClient
+  private _upperSocket: MosSocketServer
+  private _querySocket: MosSocketServer
+
+  /** */
+  constructor (conf: ConnectionConfig) {
+    this._conf = conf
+  }
+
+}
--- src/dataTypes/mosTime.ts
+++ src/dataTypes/mosTime.ts
@@ -1,39 +1,39 @@
 export default class MosTime {
 
-    private _time:Date
+  private _time: Date
 
     /** */
-    constructor (timestamp?: Date|number|string) {
-        let time: Date
-        if (timestamp) {
-            // create date from time-string or timestamp number
-            if (typeof timestamp === 'string' || typeof timestamp === 'number') {
-                time = new Date(timestamp.toString())
-                if(isNaN(time.getTime())) {
-                    throw new Error('Invalid timestamp')
-                }
-            }else{
-                // received Date object
-                // @todo: check if it is a valid Date object, or trust TS?
-                time = timestamp
-            }
-            
-        }else {
-            // no timestamp, create Date now
-            time = new Date()
+  constructor (timestamp?: Date|number|string) {
+    let time: Date
+    if (timestamp) {
+      // create date from time-string or timestamp number
+      if (typeof timestamp === 'string' || typeof timestamp === 'number') {
+        time = new Date(timestamp.toString())
+        if (isNaN(time.getTime())) {
+          throw new Error('Invalid timestamp')
         }
+      }else {
+        // received Date object
+        // @todo: check if it is a valid Date object, or trust TS?
+        time = timestamp
+      }
 
-        if(isNaN(time.getTime())) {
-            throw new Error('Invalid timestamp')
-        }
-
-        this._time = time
+    }else {
+      // no timestamp, create Date now
+      time = new Date()
     }
 
-    toString ():string {
-        let ISOstring: string = this._time.toISOString()
-
-        // remove last character (timezone identificator), to be MOS time format compliant
-        return ISOstring.slice(0, -1)
+    if (isNaN(time.getTime())) {
+      throw new Error('Invalid timestamp')
     }
+
+    this._time = time
+  }
+
+  toString (): string {
+    let ISOstring: string = this._time.toISOString()
+
+    // remove last character (timezone identificator), to be MOS time format compliant
+    return ISOstring.slice(0, -1)
+  }
 }
