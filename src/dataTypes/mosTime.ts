a7fb3f28d1592cd873c8b85c31572486150087e9
Stephan Eriksen <stephanruler@gmail.com>
2018-02-24 12:37:33 +0100

    fix: Initial src restructure
    With the new test-structure we need to move some stuff around.
    We should move away from export default

--- src/dataTypes/mosTime.ts
+++ src/dataTypes/mosTime.ts
@@ -1,74 +1,73 @@
-function pad (n: string, width: number, z?: string): string {
-  z = z || '0'
-  n = n + ''
-  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
-}
 
-export default class MosTime {
+const timestampRegex = /(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)([,\.](\d{3}))?(([+-Z])([:\d]+)?)?/
+export class MosTime {
+	private _time: Date
 
-  private _time: Date
+	constructor (timestamp?: Date | number | string) {
+		let time: Date
+		if (timestamp !== undefined) {
+			// create date from time-string or timestamp number
+			if (typeof timestamp === 'number') {
+				time = new Date(timestamp)
+				if (isNaN(time.getTime())) {
+					throw new Error('Invalid timestamp')
+				}
+			} else if (typeof timestamp === 'string') {
+				// "YYYY-MM-DD'T'hh:mm:ss[,ddd]['Z']""
 
-  /** */
-  constructor (timestamp?: Date|number|string) {
-    let time: Date
-    if (timestamp !== undefined) {
-      // create date from time-string or timestamp number
-      if (typeof timestamp === 'number') {
-        time = new Date(timestamp)
-        if (isNaN(time.getTime())) {
-          throw new Error('Invalid timestamp')
-        }
-      } else if (typeof timestamp === 'string') {
-        // "YYYY-MM-DD'T'hh:mm:ss[,ddd]['Z']""
+				let match = timestamp.match(timestampRegex)
 
-        let m = timestamp.match(/(\d+)-(\d+)-(\d+)T(\d+):(\d+):(\d+)([,\.](\d{3}))?(([+-Z])([:\d]+)?)?/)
+				if (!match) throw new Error('Invalid timestamp')
 
-        if (!m) throw new Error('Invalid timestamp')
+				let yy = MosTime.pad(match[1], 4)
+				let mm = MosTime.pad(match[2], 2)
+				let dd = MosTime.pad(match[3], 2)
 
-        let yy = pad(m[1], 4)
-        let mm = pad(m[2], 2)
-        let dd = pad(m[3], 2)
+				let hh = MosTime.pad(match[4], 2)
+				let ii = MosTime.pad(match[5], 2)
+				let ss = MosTime.pad(match[6], 2)
 
-        let hh = pad(m[4], 2)
-        let ii = pad(m[5], 2)
-        let ss = pad(m[6], 2)
+				let ms = match[8]
+				let tzSign = match[10]
+				let tzTime = match[11]
 
-        let ms = m[8]
-        let tzSign = m[10]
-        let tzTime = m[11]
+				let dateStr = yy + '-' + mm + '-' + dd + 'T' + hh + ':' + ii + ':' + ss +
+					(ms ? '.' + ms : '') +
+					(tzSign === 'Z' ? tzSign : tzTime + MosTime.pad(tzTime, 5))
 
-        let dateStr = yy + '-' + mm + '-' + dd + 'T' + hh + ':' + ii + ':' + ss +
-        (ms ? '.' + ms : '' ) +
-        (tzSign === 'Z' ? tzSign : tzTime + pad(tzTime,5))
+				time = new Date(dateStr)
+				if (isNaN(time.getTime())) {
+					throw new Error('Invalid timestamp')
+				}
+			} else {
+				// received Date object
+				// @todo: check if it is a valid Date object, or trust TS?
+				time = timestamp
+			}
 
-        time = new Date(dateStr)
-        if (isNaN(time.getTime())) {
-          throw new Error('Invalid timestamp')
-        }
-      } else {
-        // received Date object
-        // @todo: check if it is a valid Date object, or trust TS?
-        time = timestamp
-      }
+		} else {
+			// no timestamp, create Date now
+			time = new Date()
+		}
 
-    } else {
-      // no timestamp, create Date now
-      time = new Date()
-    }
+		if (isNaN(time.getTime())) {
+			throw new Error('Invalid timestamp')
+		}
 
-    if (isNaN(time.getTime())) {
-      throw new Error('Invalid timestamp')
-    }
+		this._time = time
+	}
+	private static pad (n: string, width: number, z?: string): string {
+		z = z || '0'
+		n = n + ''
+		return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
+	}
+	toString (): string {
+		let ISOstring = this._time.toISOString()
 
-    this._time = time
-  }
-  toString (): string {
-    let ISOstring: string = this._time.toISOString()
-
-    // remove last character (timezone identificator), to be MOS time format compliant
-    return ISOstring.slice(0, -1)
-  }
-  getTime (): number {
-    return this._time.getTime()
-  }
+		// remove last character (timezone identificator), to be MOS time format compliant
+		return ISOstring.slice(0, -1)
+	}
+	getTime (): number {
+		return this._time.getTime()
+	}
 }
