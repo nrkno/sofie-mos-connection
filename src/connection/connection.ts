d717c7140b6f599b9283a774cf8d4ddaf93285c1
Jesper Stærkær <jesper@starkar.no>
2018-02-25 13:29:53 +0100

    refactor: Relocated and grouped connection classes

---
+++ src/connection/connection.ts
@@ -0,0 +1,10 @@
+import { MosSocketClient } from './mosSocketClient'
+import { MosSocketServer } from './mosSocketServer'
+
+export class Connection {
+
+	/** */
+	constructor (serverOrClient: MosSocketClient | MosSocketServer) {
+
+	}
+}
