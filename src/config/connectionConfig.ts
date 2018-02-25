7bf71076921a248ea8f9509349c355f816daf964
Jesper Stærkær <jesper@starkar.no>
2018-02-25 11:32:26 +0100

    feat(ConnectionConfig): Adds profile validation for compliancy in profile combinations

--- src/config/connectionConfig.ts
+++ src/config/connectionConfig.ts
@@ -1,25 +1,114 @@
-export class ConnectionConfig {
+export class ConnectionConfig implements IConnectionConfig {
 	mosID: string
-	ncs: {
-		ncsID: string,
-		host: string
-	}
-	ncsBuddy?: {
-		ncsID: string,
-		host: string
-	}
-	profiles: {
-		'0': boolean,
-		'1'?: boolean,
-		'2'?: boolean,
-		'3'?: boolean,
-		'4'?: boolean,
-		'5'?: boolean,
-		'6'?: boolean,
-		'7'?: boolean
+	acceptsConnections: boolean
+	accepsConnectionsFrom: string[]
+
+	private _profiles: ProfilesSupport = {
+		'0': true,
+		'1': true,
+		'2': true,
+		'3': true,
+		'4': true,
+		'5': true,
+		'6': true,
+		'7': true
 	}
 
-	// @todo: resolve profile dependecies
+	constructor (init: IConnectionConfig) {
+		Object.assign(this, init)
+	}
+
+	/** */
+	get profiles (): ProfilesSupport {
+		return this._profiles
+	}
+
+	/** */
+	set profiles (profileSupport: ProfilesSupport) {
+		let atLeastOneOtherProfile = false
+
+		// profile 0 is mandatory
+		if (profileSupport['0'] === true) {
+			this._profiles['0'] = true
+		} else {
+			throw new Error(`Invalid MOS configuration: Profile 0 is mandatory.`)
+		}
+
+		// Profile 1 depends on 0
+		if (profileSupport['1'] === true) {
+			if (!this._profiles['0'] === true) {
+				throw new Error(`Invalid MOS configuration: Profile 1 depends on Profile 0.`)
+			} else {
+				this._profiles['1'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 2 depends on 0 and 1
+		if (profileSupport['2'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 2 depends on Profile 0 and 1.`)
+			} else {
+				this._profiles['2'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 3 depends on 0, 1 and 2
+		if (profileSupport['3'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true && this._profiles['2'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 3 depends on Profile 0, 1 and 2.`)
+			} else {
+				this._profiles['3'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 4 depends on 0, 1 and 2
+		if (profileSupport['4'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true && this._profiles['2'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 4 depends on Profile 0, 1 and 2.`)
+			} else {
+				this._profiles['4'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 5 depends on 0, 1 and 2
+		if (profileSupport['5'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true && this._profiles['2'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 5 depends on Profile 0, 1 and 2.`)
+			} else {
+				this._profiles['5'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 6 depends on 0, 1 and 2
+		if (profileSupport['6'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true && this._profiles['2'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 6 depends on Profile 0, 1 and 2.`)
+			} else {
+				this._profiles['6'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// Profile 7 depends on 0, 1 and 2
+		if (profileSupport['7'] === true) {
+			if (!(this._profiles['0'] === true && this._profiles['1'] === true && this._profiles['2'] === true)) {
+				throw new Error(`Invalid MOS configuration: Profile 7 depends on Profile 0, 1 and 2.`)
+			} else {
+				this._profiles['7'] = true
+				atLeastOneOtherProfile = true
+			}
+		}
+
+		// must support at least one other profile, other than Profile 0
+		if (!atLeastOneOtherProfile) {
+			throw new Error(`Invalid MOS configuration: Mos device must support at least one profile other than the mandantory Profile 0.`)
+		}
+	}
 
 	// machineInfo: {
 	// 	manufacturer: "SuperFly.tv",
@@ -34,4 +123,24 @@ export class ConnectionConfig {
 	//     <mosRev>2.8.2</mosRev>
 	//     */
 	// }
+}
+
+/** */
+export interface IConnectionConfig {
+	mosID: string
+	acceptsConnections: boolean
+	accepsConnectionsFrom: string[]
+	profiles: ProfilesSupport
+}
+
+/** */
+export class ProfilesSupport {
+	'0': boolean = true
+	'1'?: boolean
+	'2'?: boolean
+	'3'?: boolean
+	'4'?: boolean
+	'5'?: boolean
+	'6'?: boolean
+	'7'?: boolean
 }
