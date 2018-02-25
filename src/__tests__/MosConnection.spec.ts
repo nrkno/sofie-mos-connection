97e2c4e4049c7c8611951937dd112d7663245087
Johan Nyman <johan@nytamin.se>
2018-02-25 10:48:00 +0100

    feat: added basic MosConnection test

--- src/__tests__/MosConnection.spec.ts
+++ src/__tests__/MosConnection.spec.ts
@@ -1,4 +1,41 @@
 
-test('Simple test', () => {
-	expect(2).toBe(2)
+import {MosConnection} from "../MosConnection"
+
+
+test('Mos profile 0', async () => {
+
+	// Test test:
+	const mos = new MosConnection({
+		acceptConnections: true, // default:true
+		acceptConnectionFrom: ['127.0.0.1'],
+		
+		profiles: {
+			'0': true,
+		}
+	});
+
+	expect(mos).toBeInstanceOf(MosConnection);
+
+	var onConnection = jest.fn(() => {
+		// a new connection has been made
+	});
+
+	mos.onConnection(onConnection);
+
+
+	// Connect to ENPS:
+	await mos.connect({
+		ncs: {
+			ncsID: "MYTESTSERVER",
+			host: "127.0.0.1"
+		},
+		/*ncsBuddy?: {
+			ncsID: string;
+			host: string;
+		},*/
+	})
+
+	expect(onConnection).toHaveBeenCalled();
+
+	return 0;
 })
