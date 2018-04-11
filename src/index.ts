9cd88ea9eba4fa74d06a82c397ec2b119ad649b9
Jonatan Hjalmarsson <jonatan@atellus.se>
2018-04-11 09:11:15 +0200

    Really export IConnectionConfig

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 
 export * from './MosConnection'
 export * from './api'
-export {ConnectionConfig} from './config/connectionConfig'
+export {
+	IConnectionConfig,
+	ConnectionConfig
+} from './config/connectionConfig'
 
 export {MosString128} from './dataTypes/mosString128'
 export {MosDuration} from './dataTypes/mosDuration'
