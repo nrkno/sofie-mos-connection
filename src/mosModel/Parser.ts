8b3497df64f1a62d231c2f5c9a2e82c53867a9e6
Johan Nyman <johan@nytamin.se>
2018-04-13 11:03:53 +0200

    feat: stub for parser, WIP

---
+++ src/mosModel/Parser.ts
@@ -0,0 +1,26 @@
+import * as XMLBuilder from 'xmlbuilder'
+import {
+	IMOSRunningOrder,
+	IMOSROStory,
+	IMOSItem,
+	IMOSExternalMetaData
+} from '../api'
+export namespace Parser {
+
+	function xml2RO (xml: XMLBuilder.XMLElementOrXMLNode): IMOSRunningOrder {
+	}
+	function ro2xml (ro: IMOSRunningOrder): XMLBuilder.XMLElementOrXMLNode {
+	}
+	function xml2Story (xml: XMLBuilder.XMLElementOrXMLNode): IMOSROStory {
+	}
+	function story2xml (story: IMOSROStory): XMLBuilder.XMLElementOrXMLNode {
+	}
+	function xml2Item (xml: XMLBuilder.XMLElementOrXMLNode): IMOSItem {
+	}
+	function item2xml (item: IMOSItem): XMLBuilder.XMLElementOrXMLNode {
+	}
+	function xml2MetaData (xml: XMLBuilder.XMLElementOrXMLNode): IMOSExternalMetaData {
+	}
+	function metaData2xml (item: IMOSExternalMetaData): XMLBuilder.XMLElementOrXMLNode {
+	}
+}
