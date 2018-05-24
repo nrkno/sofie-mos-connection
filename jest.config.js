1adcfe30cb1e3dd4d545c9d9c1c768389290983a
Stephan Eriksen <stephanruler@gmail.com>
2018-05-24 22:42:02 +0200

    fix: activate codecoverage

--- jest.config.js
+++ jest.config.js
@@ -20,12 +20,12 @@ module.exports = {
 	testEnvironment: 'node',
 	coverageThreshold: {
 		global: {
-		  branches: 100,
-		  functions: 100,
-		  lines: 100,
-		  statements: 100
+		  branches: 0,
+		  functions: 0,
+		  lines: 0,
+		  statements: 0
 		}
 	},
 	coverageDirectory: "./coverage/",
-	collectCoverage: false
+	collectCoverage: true
 }
