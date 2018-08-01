b3dc170ba89201a633d87c8c1553074aca9b0f18
Unknown <balte.dewit@gmail.com>
2018-08-01 15:07:03 +0200

    fix: single child xml data

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -10,11 +10,13 @@ export function pad (n: string, width: number, z?: string): string {
 export function xml2js (messageString: string): object {
 	let object = xmlParser(messageString, { compact: false, trim: true, nativeType: true })
 	// common tags we typically want to know the order of the contents of:
-	const orderedTags = new Set([ 'mosAbstract', 'description', 'p', 'em', 'span', 'h1', 'h2', 'i', 'b' ])
+	const orderedTags = new Set([ 'storyBody', 'storyItem', 'mosAbstract', 'description', 'p', 'em', 'span', 'h1', 'h2', 'i', 'b' ])
 
 	const concatChildrenAndTraverseObject = (element: {[key: string]: any }) => {
 		if (element.elements) {
 			if (element.elements.length === 1) {
+				concatChildrenAndTraverseObject(element.elements[0])
+
 				const childEl = element.elements[0]
 				const name = childEl.name || childEl.type || 'unknownElement'
 				if (childEl.type && childEl.type === 'text') {
@@ -30,7 +32,6 @@ export function xml2js (messageString: string): object {
 					)
 				}
 				delete element.elements
-				concatChildrenAndTraverseObject(childEl)
 				if (childEl.type === 'text') {
 					element[name] = childEl.text
 					if (childEl.attributes) {
@@ -45,83 +46,86 @@ export function xml2js (messageString: string): object {
 					concatChildrenAndTraverseObject(childEl)
 				}
 
-				let names: Array<string> = element.elements.map((obj: { name?: string, type?: string }) => obj.name || obj.type || 'unknownElement')
-				let namesSet = new Set(names)
-				if ((namesSet.size === 1 && names.length !== 1) && !namesSet.has('type') && !namesSet.has('name')) {
-					// make array compact:
-					const array: any = []
-					for (const childEl of element.elements) {
-						if (childEl.type && childEl.type === 'text') {
-							if (Object.keys(childEl).length > 2) {
-								array.push(childEl)
-							} else if (childEl.attributes) {
-								childEl.attributes.text = childEl.text
-								array.push(childEl.attributes)
+				if (!orderedTags.has(element.name)) {
+					// console.log(element)
+					let names: Array<string> = element.elements.map((obj: { name?: string, type?: string }) => obj.name || obj.type || 'unknownElement')
+					let namesSet = new Set(names)
+					if ((namesSet.size === 1 && names.length !== 1) && !namesSet.has('type') && !namesSet.has('name')) {
+						// make array compact:
+						const array: any = []
+						for (const childEl of element.elements) {
+							if (childEl.type && childEl.type === 'text') {
+								if (Object.keys(childEl).length > 2) {
+									array.push(childEl)
+								} else if (childEl.attributes) {
+									childEl.attributes.text = childEl.text
+									array.push(childEl.attributes)
+								} else {
+									array.push(childEl.text)
+								}
 							} else {
-								array.push(childEl.text)
+								if (childEl.type) delete childEl.type
+								if (childEl.name) delete childEl.name
+								if (Object.keys(childEl).length > 1) {
+									// might contain something useful like attributes
+									if (childEl.attributes) {
+										for (const key in childEl.attributes) {
+											childEl[key] = childEl.attributes[key]
+										}
+										delete childEl.attributes
+									}
+									array.push(childEl)
+								} else {
+									array.push(childEl[Object.keys(childEl)[0]])
+								}
 							}
-						} else {
-							if (childEl.type) delete childEl.type
-							if (childEl.name) delete childEl.name
-							if (Object.keys(childEl).length > 1) {
-								// might contain something useful like attributes
+						}
+						element[names[0]] = array
+						delete element.elements
+					} else if (names.length === namesSet.size) {
+						for (const childEl of element.elements) {
+							if (childEl.type && childEl.type === 'text' && (Object.keys(childEl).length < 4 || (!childEl.name && Object.keys(childEl).length < 3))) {
+								if (!childEl.text) {
+									element.text = childEl.text
+								}
+								element[childEl.name] = childEl.text
+							} else {
 								if (childEl.attributes) {
 									for (const key in childEl.attributes) {
 										childEl[key] = childEl.attributes[key]
 									}
 									delete childEl.attributes
 								}
-								array.push(childEl)
-							} else {
-								array.push(childEl[Object.keys(childEl)[0]])
+								const name = childEl.name || childEl.type || 'unknownEl'
+								if (childEl.type) delete childEl.type
+								if (childEl.name) delete childEl.name
+								element[name] = childEl
 							}
 						}
-					}
-					element[names[0]] = array
-					delete element.elements
-				} else if (names.length === namesSet.size && !orderedTags.has(element.name)) {
-					for (const childEl of element.elements) {
-						if (childEl.type && childEl.type === 'text' && (Object.keys(childEl).length < 4 || (!childEl.name && Object.keys(childEl).length < 3))) {
-							if (!childEl.text) {
-								element.text = childEl.text
-							}
-							element[childEl.name] = childEl.text
-						} else {
-							if (childEl.attributes) {
+						delete element.elements
+					} else if (names.length !== namesSet.size) {
+						const holder: {[key: string]: any} = {}
+						for (let childEl of element.elements) {
+							const name = childEl.name
+							if (childEl.type === 'text' && Object.keys(childEl).length < 4) {
+								childEl = childEl.text
+							} else if (childEl.attributes) {
 								for (const key in childEl.attributes) {
 									childEl[key] = childEl.attributes[key]
 								}
 								delete childEl.attributes
 							}
-							const name = childEl.name || childEl.type || 'unknownEl'
-							if (childEl.type) delete childEl.type
-							if (childEl.name) delete childEl.name
-							element[name] = childEl
-						}
-					}
-					delete element.elements
-				} else if (names.length !== namesSet.size && !orderedTags.has(element.name)) {
-					const holder: {[key: string]: any} = {}
-					for (let childEl of element.elements) {
-						const name = childEl.name
-						if (childEl.type === 'text' && Object.keys(childEl).length < 4) {
-							childEl = childEl.text
-						} else if (childEl.attributes) {
-							for (const key in childEl.attributes) {
-								childEl[key] = childEl.attributes[key]
+							if (holder[name]) {
+								holder[name].push(childEl)
+							} else {
+								holder[name] = [ childEl ]
 							}
-							delete childEl.attributes
 						}
-						if (holder[name]) {
-							holder[name].push(childEl)
-						} else {
-							holder[name] = [ childEl ]
+						for (const key in holder) {
+							element[key] = holder[key].length > 1 ? holder[key] : holder[key][0]
 						}
+						delete element.elements
 					}
-					for (const key in holder) {
-						element[key] = holder[key].length > 1 ? holder[key] : holder[key][0]
-					}
-					delete element.elements
 				}
 			}
 		}
--- src/utils/__tests__/Utils.spec.ts
+++ src/utils/__tests__/Utils.spec.ts
@@ -3,16 +3,28 @@ import {
 } from '../Utils'
 test('xml2js', () => {
 	let o = xml2js(`
-<mos>
-<p>Hello and welcome</p>
-<mosObject>[name sign]</mosObject>
-<p>This is a extra broadcast bla bla</p>
-<mosObject></mosObject>
-<p>Let's look at this video:</p>
-<mosObject>[video]</mosObject>
-</mos>
+<roStorySend>
+	<storyBody>
+		<p>Hello and welcome</p>
+		<mosObject>[name sign]</mosObject>
+		<p>This is a extra broadcast bla bla</p>
+		<mosObject>
+		</mosObject>
+		<p>Let's look at this video:</p>
+		<mosObject>[video]</mosObject>
+	</storyBody>
+</roStorySend>
 	`)
-	// console.log(o.mos)
+	console.log(o.roStorySend.storyBody)
 	expect(o).toBeTruthy()
+	expect(o.roStorySend.storyBody).toMatchObject({ elements: 
+         [ { type: 'text', name: 'p', text: 'Hello and welcome' },
+           { type: 'text', name: 'mosObject', text: '[name sign]' },
+           { type: 'text',
+             name: 'p',
+             text: 'This is a extra broadcast bla bla' },
+           { type: 'element', name: 'mosObject' },
+           { type: 'text', name: 'p', text: 'Let\'s look at this video:' },
+           { type: 'text', name: 'mosObject', text: '[video]' } ] })
 	// expect(o.mos).toBeTruthy()
 })
