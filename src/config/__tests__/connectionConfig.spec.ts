e62608999fc7364fe9de81d018cc167f9b444a17
Johan Nyman <johan@nytamin.se>
2018-11-02 09:43:29 +0100

    chore: connectionConfig test

--- src/config/__tests__/connectionConfig.spec.ts
+++ src/config/__tests__/connectionConfig.spec.ts
@@ -23,4 +23,69 @@ describe('ConnectionConfig', () => {
 		expect(config.openRelay).toEqual(false)
 		expect(config.offspecFailover).toEqual(false)
 	})
+
+	test('invalid ConnectionConfig', () => {
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig()
+		}).toThrowError(/object.*missing/)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig(1)
+		}).toThrowError(/not an object/)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig({
+				mosID: 'test',
+				acceptsConnections: true
+			})
+		}).toThrowError(/profiles.*missing/)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig({
+				mosID: 'test',
+				profiles: {
+					'0': true,
+					'1': true
+				}
+			})
+		}).toThrowError(/acceptsConnections.*missing/)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig({
+				acceptsConnections: true,
+				profiles: {
+					'0': true,
+					'1': true
+				}
+			})
+		}).toThrowError(/mosID.*missing/)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig({
+				mosID: 'test',
+				acceptsConnections: true,
+				profiles: {
+					'1': true
+				}
+			})
+		}).toThrowError(/profile.*0.*mandatory/i)
+
+		expect(() => {
+			// @ts-ignore
+			const config = new ConnectionConfig({
+				mosID: 'test',
+				acceptsConnections: true,
+				profiles: {
+					'0': true
+				}
+			})
+		}).toThrowError(/must support at least one profile/i)
+	})
 })
